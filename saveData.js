8720747a217819b2ec0a8be12adc1ea7e1f3ce14
Quenteon <quanteonquantumelysium@gmail.com>
2024-12-13 23:21:06 +0000

    prettier for save data

--- saveData.js
+++ saveData.js
@@ -2,24 +2,36 @@
 const { createConnection } = require("./db/mysql/connector");
 
 const saveDataToDb = async (processedData, service) => {
+  let connector;
   try {
-    const connector = await createConnection();
+    // open
+    connector = await createConnection();
 
-    const values = processedData.items.map(item => {
+    // collect all data
+    const values = processedData.items.map((item) => {
       const { title, link, pubDate, traffic, picture, newsItems } = item;
       const interest = traffic;
-      const source_timestamp = new Date(pubDate).toISOString().slice(0, 19).replace('T', ' ');
-      const { service_id, language_code, language_name, country_code, country_name } = service;
-      
+      const source_timestamp = new Date(pubDate)
+        .toISOString()
+        .slice(0, 19)
+        .replace("T", " ");
+      const {
+        service_id,
+        language_code,
+        language_name,
+        country_code,
+        country_name,
+      } = service;
+
       return [
-        service_id, 
-        language_code, 
-        language_name, 
-        country_code, 
-        country_name, 
-        title, 
-        interest, 
-        source_timestamp
+        service_id,
+        language_code,
+        language_name,
+        country_code,
+        country_name,
+        title,
+        interest,
+        source_timestamp,
       ];
     });
 
@@ -30,30 +42,35 @@ const saveDataToDb = async (processedData, service) => {
       VALUES ?
     `;
 
+    // batch
     return new Promise((resolve, reject) => {
       connector.query(query, [values], (err, result) => {
         if (err) {
           reject({
             success: false,
-            message: 'Error saving data',
+            message: "Error saving data",
             error: err,
           });
         } else {
           resolve({
             success: true,
             message: `Data saved. Inserted ${result.affectedRows} rows.`,
-            insertedId: result.insertId || null, 
+            insertedId: result.insertId || null,
           });
         }
       });
     });
-
   } catch (err) {
     return {
       success: false,
-      message: 'Connection error',
+      message: "Connection error",
       error: err,
     };
+  } finally {
+    // every time close connection
+    if (connector) {
+      connector.end();
+    }
   }
 };
 
