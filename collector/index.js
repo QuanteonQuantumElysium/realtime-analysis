95a80a88fa0633bf813e95ecf349cbd8aaf95045
Quenteon <quanteonquantumelysium@gmail.com>
2024-12-13 23:43:46 +0000

    transfer to folder

---
+++ collector/index.js
@@ -0,0 +1,34 @@
+const { getSourceServiceData } = require("../db/mysql/connector");
+const fetchRSS = require("./fetchRss");
+const parseXML = require("./parseXML");
+const processFeed = require("./processFeed");
+const saveDataToDb = require("./saveData");
+
+const collector = async () => {
+  try {
+    const services = await getSourceServiceData();
+
+    if (services.length === 0) {
+      console.log("No services available.");
+      return;
+    }
+
+    for (let service of services) {
+      if (service.access_type_id === 2 || service.data_format_code === "xml") {
+        const xmlData = await fetchRSS(service.full_url);
+        if (xmlData) {
+          const jsonData = await parseXML(xmlData);
+          const processedData = processFeed(jsonData);
+          const result = await saveDataToDb(processedData, service);
+          console.log(`${service.full_url}`, result);
+        } else {
+          console.error(`Failed to fetch RSS data from ${service.full_url}`);
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Main error:", error);
+  }
+};
+
+module.exports = collector
